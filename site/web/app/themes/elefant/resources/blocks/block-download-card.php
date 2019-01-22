16cf798b7bc000a0cca4ea113ff86d058d482405
linkslegend <rd.intermac@googlemail.com>
2019-01-22 18:13:43 +0100

    custom guteberg block

---
+++ site/web/app/themes/elefant/resources/blocks/block-download-card.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <img class="card-img-top" src="<?php block_field( 'image-url' ); ?>" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title"><?php block_field( 'card-title' ); ?></h5>
+      <p class="card-text"><?php block_field( 'card-text' ); ?></p>
+    </div>
+    <div class="card-footer">
+      <a href="<?php block_field( 'button-url' ); ?>"><?php block_field( 'button-text' ); ?></a>
+    </div>
+  </div>
