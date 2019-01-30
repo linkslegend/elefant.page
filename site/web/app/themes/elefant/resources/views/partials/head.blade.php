d945284869a4f89dae9fc4a5e9d1ddf2a200a97e
linkslegend <rd.intermac@googlemail.com>
2019-01-30 13:12:21 +0100

    serviceworker update

--- site/web/app/themes/elefant/resources/views/partials/head.blade.php
+++ site/web/app/themes/elefant/resources/views/partials/head.blade.php
@@ -5,18 +5,13 @@
   <meta name="theme-color" content="#cce8f0">
   <script>
       // register service worker:
-      if (navigator.serviceWorker) {
-        navigator.serviceWorker.register('/app/themes/elefant/serviceworker.js').then(function() {
-          return navigator.serviceWorker.ready;
-        })
-        .then(function(registration) {
-          console.log(registration); // service worker is ready and working...
-        });
-
-        navigator.serviceWorker.addEventListener('message', function(event) {
-          console.log(event.data.message); // Hello World !
-        });
-      }
+      (function () {
+          if ('serviceWorker' in navigator) {
+              window.addEventListener('load', function () {
+                  navigator.serviceWorker.register('/app/themes/elefant/serviceworker.js');
+              });
+          }
+      })();
     </script>
   <link rel="manifest" href="/app/themes/elefant/manifest.json">
 
