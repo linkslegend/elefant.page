dc7d4c41844327a3466aeceee4c9623b79876884
linkslegend <rd.intermac@googlemail.com>
2019-02-18 19:18:45 +0100

    small update

--- site/web/app/themes/elefant/app/setup.php
+++ site/web/app/themes/elefant/app/setup.php
@@ -44,7 +44,7 @@ add_action( 'wp_enqueue_scripts',  function() {
 function jk_dequeue_styles( $enqueue_styles ) {
 	  // unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
     // unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
-	  unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
+	     unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
 	return $enqueue_styles;
 }
 add_filter( 'woocommerce_enqueue_styles', __NAMESPACE__ . '\\jk_dequeue_styles' );
