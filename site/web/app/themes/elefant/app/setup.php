41c728bb2d628ffd7c2c6b57e0ef13a45c7c69b8
Robin <rd.intermac@googlemail.com>
2019-05-27 15:55:54 +0200

    test

--- site/web/app/themes/elefant/app/setup.php
+++ site/web/app/themes/elefant/app/setup.php
@@ -222,7 +222,7 @@ add_action('after_setup_theme', function () {
     add_theme_support('soil-relative-urls');
     add_theme_support('soil-js-to-footer');
     add_theme_support( 'wc-product-gallery-zoom' );
-    add_theme_support( 'wc-product-gallery-lightbox' );
+    //add_theme_support( 'wc-product-gallery-lightbox' );
     add_theme_support( 'wc-product-gallery-slider' );
 
     /**
