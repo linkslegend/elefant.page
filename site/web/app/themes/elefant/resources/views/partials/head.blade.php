a6fbb84fc22162f7ab6c931cad23d8e719665b2b
Robin <rd.intermac@googlemail.com>
2019-06-28 15:32:11 +0200

    test

--- site/web/app/themes/elefant/resources/views/partials/head.blade.php
+++ site/web/app/themes/elefant/resources/views/partials/head.blade.php
@@ -47,4 +47,19 @@
 @endif
 
   @php wp_head() @endphp
+
+  <script>
+    // register service worker:
+            if ('serviceWorker' in navigator) {
+              window.addEventListener('load', function() {
+                navigator.serviceWorker.register('https://elefant.page/serviceworker.js').then(function(registration) {
+                  // Registration was successful
+                  console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                }, function(err) {
+                  // registration failed :(
+                  console.log('ServiceWorker registration failed: ', err);
+                });
+              });
+            }
+    </script>
 </head>
