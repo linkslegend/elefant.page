fbfc50bae3ab11db438fcb10f4da0f9598617c50
Robin <rd.intermac@googlemail.com>
2019-07-02 14:47:22 +0200

    admin fix

--- site/web/app/themes/elefant/app/filters.php
+++ site/web/app/themes/elefant/app/filters.php
@@ -88,6 +88,10 @@ add_filter('comments_template', function ($comments_template) {
  */
 add_filter('wp_get_attachment_image_attributes', function ($attr, $attachment) {
     // Bail on admin
+    if (is_admin()) {
+        return $attr;
+    }
+
     $attr['data-src'] = $attr['src'];
     $attr['data-srcset'] = $attr['srcset'];
     $attr['class'] .= ' lozad';
