111eab0ae049816c7260a9669ccc9595507249df
Robin <rd.intermac@googlemail.com>
2019-07-03 12:54:15 +0200

    test

--- site/web/app/themes/elefant/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/elefant/resources/assets/scripts/routes/common.js
@@ -56,266 +56,266 @@ export default {
     });
   });
 
-  $(window).on('load', function() {
-    $('.multiple-items').slick({
-      infinite: false,
-      dots: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
-      nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
-      responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ],
-    });
-
-    $('.multiple-items-top').slick({
-      infinite: true,
-      dots: false,
-      lazyLoad: 'ondemand',
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
-      nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
-      responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ],
-    });
-
-    $('.product-slider').slick({
-      infinite: false,
-      dots: false,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
-      nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
-      responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ],
-    });
-
-    $('.columns-8').slick({
-      infinite: false,
-      dots: false,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
-      nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
-      responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ],
-    });
-
-    $('.div-li-slider').slick({
-      infinite: false,
-      dots: false,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
-      nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
-      responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ],
-    });
-
-    $('.product-slider-frontpage').slick({
-      infinite: true,
-      dots: false,
-      slidesToShow: 4,
-      lazyLoad: 'ondemand',
-      slidesToScroll: 1,
-      prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
-      nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
-      responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ],
-    });
-
-    $('.logo-slider-frontpage').slick({
-      infinite: false,
-      dots: false,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
-      nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
-      responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-    ],
-    });
-  });
-
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
+
+    $(window).on('load', function() {
+      $('.multiple-items').slick({
+        infinite: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
+        nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
+        responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+      ],
+      });
+  
+      $('.multiple-items-top').slick({
+        infinite: true,
+        dots: false,
+        lazyLoad: 'ondemand',
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
+        nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
+        responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+      ],
+      });
+  
+      $('.product-slider').slick({
+        infinite: false,
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
+        nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
+        responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+      ],
+      });
+  
+      $('.columns-8').slick({
+        infinite: false,
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
+        nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
+        responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+      ],
+      });
+  
+      $('.div-li-slider').slick({
+        infinite: false,
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
+        nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
+        responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+      ],
+      });
+  
+      $('.product-slider-frontpage').slick({
+        infinite: true,
+        dots: false,
+        slidesToShow: 4,
+        lazyLoad: 'ondemand',
+        slidesToScroll: 1,
+        prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
+        nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
+        responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+      ],
+      });
+  
+      $('.logo-slider-frontpage').slick({
+        infinite: false,
+        dots: false,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        prevArrow:'<button class="prev"><i class="fas fa-arrow-left"></i>Previous</button>',
+        nextArrow:'<button class="next"><i class="fas fa-arrow-right"></i>Next</button>',
+        responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+      ],
+      });
+    });
   },
 };
